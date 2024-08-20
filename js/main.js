00a2b2e5619de6d76eb8f9de256d010a1599bf1c
juansantos <juan.pablo.santos.2014@gmail.com>
2024-08-20 20:28:01 -0300

    arreglo del codigo

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const PRODUCTOS = [
     {
         id: 'jordan 1',
         titulo: 'Air Jordan 1',
-        imagen: '/img/jordan/nike 2.jpg',
+        imagen: '../img/jordan/nike 2.jpg',
         categorias: {
             nombre: 'zapatillas Jordan',
             id: 'jordan',
@@ -13,7 +13,7 @@ const PRODUCTOS = [
     {
         id: 'jordan 2',
         titulo: 'Air Jordan 2',
-        imagen: '/img/jordan/nike3.jpg',
+        imagen: '../img/jordan/nike3.jpg',
         categorias: {
             nombre: 'zapatillas Jordan',
             id: 'jordan',
@@ -23,7 +23,7 @@ const PRODUCTOS = [
     {
         id: 'jordan 3',
         titulo: 'Air Jordan 3',
-        imagen: '/img/jordan/nike 5.webp',
+        imagen: '../img/jordan/nike 5.webp',
         categorias: {
             nombre: 'zapatillas Jordan',
             id: 'jordan',
@@ -35,9 +35,9 @@ const PRODUCTOS = [
     {
         id: 'dunk 1',
         titulo: 'Nike Dunk 1',
-        imagen: '/img/dunk/dunk1.jpg',
+        imagen: '../img/dunk/dunk1.jpg',
         categorias: {
-            nombre: ' zapatillas Dunk',
+            nombre: 'zapatillas Dunk',
             id: 'dunk',
         },
         precio: 20000 
@@ -45,7 +45,7 @@ const PRODUCTOS = [
     {
         id: 'dunk 2',
         titulo: 'Nike Dunk 2',
-        imagen: '/img/dunk/dunk2.webp',
+        imagen: '../img/dunk/dunk2.webp',
         categorias: {
             nombre: 'zapatillas Dunk',
             id: 'dunk',
@@ -55,7 +55,7 @@ const PRODUCTOS = [
     {
         id: 'dunk 3',
         titulo: 'Nike Dunk 3',
-        imagen: '/img/dunk/dunk3.jpeg',
+        imagen: '../img/dunk/dunk3.jpeg',
         categorias: {
             nombre: 'zapatillas Dunk',
             id: 'dunk',
@@ -68,33 +68,26 @@ const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonCategoria = document.querySelectorAll(".boton-categoria");
 const tituloPrincipal = document.querySelector("#titulo-principal");
 let botonesAgregar = document.querySelectorAll(".producto-agregar");
-const numerito = document.querySelector ("#numerito");
-
-
+const numerito = document.querySelector("#numerito");
 
 function cargarProductos(productosElegidos) {
-
     contenedorProductos.innerHTML = ""; 
 
     productosElegidos.forEach(producto => {
-
         const div = document.createElement("div");
         div.classList.add("producto");
         div.innerHTML = `
-          <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
-          <div class="producto-detalles">
-              <h3 class="producto-titulo">${producto.titulo}</h3>
-              <p class="producto-precio">$${producto.precio}</p>
-              <button class="producto-agregar" id="${producto.id}">agregar</button>
-          </div>
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">$${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
         `;
         contenedorProductos.append(div);
     });
 
-    actualizarBotonesAgregar ()
- 
-;
-   
+    actualizarBotonesAgregar();
 }
 
 cargarProductos(PRODUCTOS);
@@ -102,72 +95,48 @@ cargarProductos(PRODUCTOS);
 botonCategoria.forEach(boton => {
     boton.addEventListener("click", (e) => {
         botonCategoria.forEach(boton => boton.classList.remove("active"));
-        
         e.currentTarget.classList.add("active");
 
-        if (e.currentTarget.id != "todos"){
-          
-            const productoCategorias = PRODUCTOS.find (producto => producto.categorias.id === e.currentTarget.id);
-                       
-            tituloPrincipal.innerText = productoCategorias.categorias.nombre;
-
-        const productosBoton = PRODUCTOS.filter(producto => producto.categorias.id === e.currentTarget.id);
-        cargarProductos(productosBoton);
-        }else {
-
+        if (e.currentTarget.id !== "todos") {
+            const productosBoton = PRODUCTOS.filter(producto => producto.categorias.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productosBoton.length > 0 ? productosBoton[0].categorias.nombre : "No hay productos";
+            cargarProductos(productosBoton);
+        } else {
             tituloPrincipal.innerText = "Todas las zapatillas";
             cargarProductos(PRODUCTOS);
-
         } 
     });
 });
 
-function actualizarBotonesAgregar (){
-
+function actualizarBotonesAgregar() {
     botonesAgregar = document.querySelectorAll(".producto-agregar");
     botonesAgregar.forEach(boton => {
-        boton.addEventListener("click", agregarAlCarrito)
+        boton.addEventListener("click", agregarAlCarrito);
     });
 }
 
-let productosEnCarrito;
+let productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito")) || [];
 
-let productosEnCarritoLS = localStorage.getItem("producto-en-carrito")
-
-
-
-    if (productosEnCarritoLS){
-         productosEnCarrito = JSON.parse (productosEnCarritoLS);
-         actualizarNumerito ();
-    }else {
-        productosEnCarrito = [];
-
-    };
-
-
-
-function agregarAlCarrito (e){
+function agregarAlCarrito(e) {
     const idBoton = e.currentTarget.id;
-    const productoAgregado =PRODUCTOS.find (producto => producto.id === idBoton);
+    const productoAgregado = PRODUCTOS.find(producto => producto.id === idBoton);
 
-    if (productosEnCarrito.some (producto => producto.id === idBoton) ){
+    if (productosEnCarrito.some(producto => producto.id === idBoton)) {
         const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
-        productosEnCarrito[index].cantidad ++;
-
-    }else {
+        productosEnCarrito[index].cantidad++;
+    } else {
         productoAgregado.cantidad = 1;
-        productosEnCarrito.push (productoAgregado);
+        productosEnCarrito.push(productoAgregado);
     }
-    actualizarNumerito ()
-
+    
+    actualizarNumerito();
     localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
-   
 }
 
-
-function actualizarNumerito (){
-    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0 );
+function actualizarNumerito() {
+    const nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
     numerito.innerText = nuevoNumerito;
-
 }
+
+actualizarNumerito();
 
